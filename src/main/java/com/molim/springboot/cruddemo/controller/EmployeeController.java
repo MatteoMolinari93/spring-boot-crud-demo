a0b0e002d6e8ecc6569c3e07cc52ed570d94aab8
Molinari <matteomolinari93@gmail.com>
2020-10-08 11:10:17 +0200

    Added other CRUD Employee endpoints

--- src/main/java/com/molim/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/molim/springboot/cruddemo/controller/EmployeeController.java
@@ -3,8 +3,12 @@ package com.molim.springboot.cruddemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +32,22 @@ public class EmployeeController {
 	private Employee getEmployee(@PathVariable() int id) {
 		return employeeService.findById(id);
 	}
+	
+	@PostMapping()
+	private void newEmployee(@RequestBody Employee employee) {
+		employee.setId(0);
+		employeeService.save(employee);
+	}
+	
+	@PutMapping("/{id}")
+	private void updateEmployee(@RequestBody Employee employee) {
+		employeeService.save(employee);
+	}
+	
+	@DeleteMapping("/{id}")
+	private void deleteEmployee(@PathVariable() int id) {
+		employeeService.deleteById(id);
+	}
+	
 	
 }
