0ed03c712ac2eb104964d61bdc3d9a3771ca6fd4
Molinari <matteomolinari93@gmail.com>
2020-10-08 11:36:46 +0200

    Switched to JPA Data

---
+++ src/main/java/com/molim/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.molim.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.molim.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
--- src/main/java/com/molim/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/molim/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,43 @@
 package com.molim.springboot.cruddemo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.molim.springboot.cruddemo.dao.EmployeeDAO;
+import com.molim.springboot.cruddemo.dao.EmployeeRepository;
 import com.molim.springboot.cruddemo.entity.Employee;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
-	private EmployeeDAO employeeDAO;
+	private EmployeeRepository employeeRepository;
 	
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeRepository.findAll();
 	}
 
 	@Override
-	@Transactional
 	public Employee findById(int id) {
-		return employeeDAO.findById(id);
+		Optional<Employee> result = employeeRepository.findById(id);
+		Employee employee = null;
+		if(result.isPresent()) {
+			employee = result.get();
+		}
+		return employee;
 	}
 
 	@Override
-	@Transactional
 	public void save(Employee employee) {
-		employeeDAO.save(employee);
+		employeeRepository.save(employee);
 	}
 
 	@Override
-	@Transactional
 	public void deleteById(int id) {
-		employeeDAO.deleteById(id);
+		employeeRepository.deleteById(id);
 	}
 
 }
